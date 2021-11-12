c8aeb8b25c0425b7c421ce1ae97998d5c9f94e0b
Julio Farfan <juliofarfan85@gmail.com>
2021-11-12 00:11:09 -0800

    config base julio

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Home
+        </v-card-title>
+        <v-card-text>
+          <p>Title.</p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
